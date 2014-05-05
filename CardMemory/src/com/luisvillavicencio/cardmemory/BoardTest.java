a41b7eb1240ab77c062655506a8a188f1fa13b4b
Luis Villavicencio <Tavo@Luiss-MacBook-Pro.local>
2014-05-04 23:43:55 -0400

    card memory game

---
+++ CardMemory/src/com/luisvillavicencio/cardmemory/BoardTest.java
@@ -0,0 +1,27 @@
+package com.luisvillavicencio.cardmemory;
+
+import student.TestCase;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author lv23, katee93
+ *  @version 2014.05.04
+ */
+public class BoardTest
+    extends TestCase
+{
+    //Fields
+    private Board board;
+
+    //Setup
+    public void setUp()
+    {
+        board = new Board(2);
+    }
+
+
+
+}
