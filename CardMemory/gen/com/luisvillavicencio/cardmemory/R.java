1b06dcf3cf641bba100104676eb12f50f327f75c
Luis Villavicencio <Tavo@Luiss-MacBook-Pro.local>
2014-05-05 10:55:16 -0400

    Card Memory game

--- CardMemory/gen/com/luisvillavicencio/cardmemory/R.java
+++ CardMemory/gen/com/luisvillavicencio/cardmemory/R.java
@@ -33,8 +33,8 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int infoLabel=0x7f080000;
-        public static final int reset=0x7f080001;
+        public static final int infoLabel=0x7f080001;
+        public static final int reset=0x7f080000;
         public static final int shapeView=0x7f080002;
     }
     public static final class layout {
